6cad3261997715c55f33cc1927c1923b96fb1877
ZooDoz <513828621@qq.com>
2018-06-21 16:30:21 +0800

    简单修改

--- TensorFlow/SecondLearn.py
+++ TensorFlow/SecondLearn.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 
+# 没有使用gpu，所以设置忽略cpu错误
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -19,7 +23,7 @@ cross_entropy = -tf.reduce_sum(y_*tf.log(y))
 # 使用梯度下降算法，通过损失函数cross_entropy进行反向传播
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
@@ -27,6 +31,6 @@ for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-  correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-  print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
